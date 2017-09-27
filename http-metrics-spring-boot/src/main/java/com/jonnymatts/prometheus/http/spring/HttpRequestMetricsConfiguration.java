041767abb0cc4b835127d2985fd789eb470849f2
Jonny Matts <jonoamatts@gmail.com>
2017-09-27 17:07:05 +0100

    use updated http core collectors, add ability to configure histogram

--- http-metrics-spring-boot/src/main/java/com/jonnymatts/prometheus/http/spring/HttpRequestMetricsConfiguration.java
+++ http-metrics-spring-boot/src/main/java/com/jonnymatts/prometheus/http/spring/HttpRequestMetricsConfiguration.java
@@ -4,7 +4,6 @@ import com.jonnymatts.prometheus.configuration.HistogramConfiguration;
 import com.jonnymatts.prometheus.http.HttpRequestMetricFilter;
 import com.jonnymatts.prometheus.http.configuration.HistogramConfigurationParser;
 import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,25 +12,34 @@ import org.springframework.core.env.Environment;
 import java.io.File;
 import java.io.FileInputStream;
 
+import static java.lang.String.format;
 import static java.nio.charset.Charset.defaultCharset;
 
 @Configuration
 public class HttpRequestMetricsConfiguration {
 
-    @Autowired
+    static final String DEFAULT_CONFIG_FILE_PATH = "/config/config.yaml";
+
     private Environment environment;
 
+    public HttpRequestMetricsConfiguration(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
-    public FilterRegistrationBean httpMetricFilter(HttpRequestMetricFilter httpRequestMetricFilter) {
+    public FilterRegistrationBean filterRegistrationBean(HttpRequestMetricFilter httpRequestMetricFilter) {
         return new FilterRegistrationBean(httpRequestMetricFilter);
     }
 
     @Bean
     public HttpRequestMetricFilter httpRequestMetricFilter(HistogramConfigurationParser histogramConfigurationParser) {
-        final String configFileLocation = environment.getProperty("http.metrics.exporter.config.path", "/config/config.yaml");
+        final String configFileLocation = environment.getProperty("http.metrics.exporter.config.path", DEFAULT_CONFIG_FILE_PATH);
         final File file = new File(configFileLocation);
-        if(!file.exists())
+        final boolean fileDoesNotExist = !file.exists();
+        if(fileDoesNotExist && configFileLocation.equals(DEFAULT_CONFIG_FILE_PATH))
             return new HttpRequestMetricFilter();
+        if(fileDoesNotExist)
+            throw new IllegalArgumentException(format("Error configuring HTTP metric exporter: file %s does not exist", file.getAbsolutePath()));
         return new HttpRequestMetricFilter(parseFile(histogramConfigurationParser, file));
     }
 
